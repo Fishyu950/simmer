9d1c0e861a5dd9a72035b44554de85f1ac68942d
fishyu950 <golu850225@gmail.com>
2026-06-04 02:45:43 +0800

    feat: service worker 快取管理

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'simmer-v0.2';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
