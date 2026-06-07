d662b18388c6d5d0d37139296f5a89e52369e819
fishyu950 <golu850225@gmail.com>
2026-06-08 01:32:50 +0800

    excel字動重建設定

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'simmer-v0.4';
+const CACHE_NAME = 'simmer-v0.5';
 self.addEventListener('install', e => {
   self.skipWaiting();
 });
@@ -6,9 +6,15 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    ).then(() => clients.claim())
   );
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
 });
